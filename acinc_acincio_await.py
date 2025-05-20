2aa67e45f2c23b3b6389932fd1c2eda11c9fd5a2
Nurzaada <Nurzaada45@example.com>
2025-05-20 12:37:28 +0600

    first commit

---
+++ acinc_acincio_await.py
@@ -0,0 +1,15 @@
+import asyncio
+
+# async def func1():
+#     print("start1")
+#     await asyncio.sleep(1)
+#     print("end")
+# async def func2():
+#     print("start2")
+#     await asyncio.sleep(2)
+#     print("end2")
+
+# async def main():
+#     await asyncio.gather(func1(), func2())
+# asyncio.run(main())
+
